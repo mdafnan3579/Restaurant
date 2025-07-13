ff71ec1bf430202a96cb4dd50f21b3546a7ae63b
mdafnan3579 <mdafnan3579@gmail.com>
2025-07-13 12:38:41 -0400

    frontend complete file upload

--- FRONTEND/src/components/HeroSection.jsx
+++ FRONTEND/src/components/HeroSection.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const HeroSection = () => {
   return (
     <section className="heroSection" id="heroSection">
-      
+      <Navbar />
       <div className="container">
         <div className="banner">
           <div className="largeBox">
@@ -11,23 +12,23 @@ const HeroSection = () => {
           </div>
           <div className="combined_boxes">
             <div className="imageBox">
-              <img src="/hero1.png" alt="hero" />
+              <img src="./hero1.png" alt="hero" />
             </div>
             <div className="textAndLogo">
               <div className="textWithSvg">
-                <h1 className="title">Arabian </h1>
-                <h1 className="title dishes_title"> Dishes</h1>
-                <img src="/threelines.svg" alt="threelines" />
+                <h1 className="title">Food</h1>
+                <h1 className="title dishes_title">Dishes</h1>
+                <img src="./threelines.svg" alt="threelines" />
               </div>
-              <img src="/logo.svg" alt="logo" className="logo" />
+              <img className="logo" src="logo.svg" alt="logo" />
             </div>
           </div>
         </div>
         <div className="banner">
           <div className="imageBox">
-            <img src="/hero2.png" alt="hero2" />
+            <img src="hero2.png" alt="hero" />
           </div>
-          <h1 className="title dishes_title"> Dishes</h1>
+          <h1 className="title dishes_title">Dishes</h1>
         </div>
       </div>
     </section>
